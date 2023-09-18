56dc56e555afd296db023d05a7870be3db4890a3
DHM1996 <dmarkarian@fi.uba.ar>
2023-09-18 20:24:17 -0300

    FILE-TRANSFER upload file working (#2)

--- src/start_server.py
+++ src/start_server.py
@@ -1,28 +1,67 @@
 import logging
 import socket
+import os
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8080
+
 BUFFER_SIZE = 1024
+
+SERVER_FOLDER = os.getcwd() + os.sep + ".." + os.sep + "server_files" + os.sep
+
+OK = str.encode("Ok")
 
 logging.basicConfig(level=logging.INFO)
 
 
-def run():
+def create_file(file_name):
+    file = open(file_name, 'wb')
+    return file
+
+
+def create_socket():
     server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     server_socket.bind((SERVER_IP, SERVER_PORT))
     logging.info("UDP server up and listening")
+    return server_socket
 
+
+def receive_file_name(server_socket):
+    file_name, address = server_socket.recvfrom(BUFFER_SIZE)
+    logging.info(f"File name received: {file_name}")
+    server_socket.sendto(OK, address)
+    logging.info("The client is notified that the file name has been received")
+    return file_name.decode()
+
+
+def receive_file_data(file, server_socket):
+    logging.info(f"Receiving file data")
     while True:
-        message, address = server_socket.recvfrom(BUFFER_SIZE)
+        data, address = server_socket.recvfrom(BUFFER_SIZE)
+        if data.decode() == "UPLOAD_END":
+            break
+        file.write(data)
+    logging.info(f"File data received successfully")
+    server_socket.sendto(OK, address)
+    logging.info("The client is notified that the data has been received")
 
-        print("Message from Client:{}".format(message))
-        print("Client IP Address:{}".format(address))
 
-        response = str.encode("Received")
+def receive_file(server_socket):
+    file_name = receive_file_name(server_socket)
+    file = create_file(SERVER_FOLDER + file_name)
+    receive_file_data(file, server_socket)
 
-        server_socket.sendto(response, address)
+
+def execute():
+    server_socket = create_socket()
+    while True:
+        task, address = server_socket.recvfrom(BUFFER_SIZE)
+
+        if task.decode() == "UPLOAD_START":
+            logging.info(f"Upload start from {address}")
+            receive_file(server_socket)
+            logging.info("The file upload has been completed successfully")
 
 
 if __name__ == '__main__':
-    run()
+    execute()
--- src/upload.py
+++ src/upload.py
@@ -1,22 +1,119 @@
+import logging
+import os
 import socket
-from src.start_server import SERVER_IP, SERVER_PORT
+import sys
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 8080
 
 CLIENT_IP = "127.0.0.1"
 CLIENT_PORT = 8081
+
 BUFFER_SIZE = 1024
 
+CLIENT_FOLDER = os.getcwd() + os.sep + ".." + os.sep + "client_files" + os.sep
 
-def run():
+logging.basicConfig(level=logging.INFO)
+
+
+class ServerException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f"Server Error: {self.message}"
+
+
+class ClientException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f"Client Error: {self.message}"
+
+
+def create_socket():
     client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    logging.info("Socket created")
+    return client_socket
 
-    message = str.encode("Te envio un mensage")
 
-    client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+def close_socket(client_socket):
+    client_socket.close()
 
+
+def open_file(file_name):
+    logging.info(f"Trying to open file {file_name}")
+    try:
+        file = open(CLIENT_FOLDER + file_name, "rb")
+        logging.info("File opened successfully")
+    except FileNotFoundError:
+        raise ClientException(f"File not found")
+    return file
+
+
+def send_upload_start_message(client_socket):
+    logging.info("Notifying the server of a new file upload")
+    client_socket.sendto(str.encode("UPLOAD_START"), (SERVER_IP, SERVER_PORT))
+
+
+def send_upload_end_message(client_socket):
+    logging.info("Notifying the server that the upload has finished")
+    client_socket.sendto(str.encode("UPLOAD_END"), (SERVER_IP, SERVER_PORT))
+
+
+def wait_server_for_file_name_confirmation(client_socket):
+    logging.info("Waiting for file name confirmation")
     response, address = client_socket.recvfrom(BUFFER_SIZE)
+    if response.decode() != "Ok":
+        raise ServerException(response)
+    logging.info("File name confirmation received successfully")
 
-    print("Message from Server {}".format(response))
+
+def wait_server_for_file_data_confirmation(client_socket):
+    logging.info("Waiting for file data confirmation")
+    response, address = client_socket.recvfrom(BUFFER_SIZE)
+    if response.decode() != "Ok":
+        raise ServerException(response)
+    logging.info("File data confirmation received successfully")
+
+
+def send_file_name(file_name, client_socket):
+    logging.info("Sending file name to the server")
+    file_name = str.encode(file_name)
+    client_socket.sendto(file_name, (SERVER_IP, SERVER_PORT))
+    logging.info("File name has been sent successfully")
+
+
+def send_file_data(file, client_socket):
+    logging.info("Sending file data to the server")
+    while True:
+        data = file.read(BUFFER_SIZE)
+        if not data:
+            return
+        client_socket.sendto(data, (SERVER_IP, SERVER_PORT))
+
+
+def upload_file(file_name: str):
+    file = open_file(file_name)
+    client_socket = create_socket()
+    send_upload_start_message(client_socket)
+    send_file_name(file_name, client_socket)
+    wait_server_for_file_name_confirmation(client_socket)
+    send_file_data(file, client_socket)
+    send_upload_end_message(client_socket)
+    wait_server_for_file_data_confirmation(client_socket)
+    close_socket(client_socket)
+
+
+def execute():
+    argv = sys.argv
+    file_name = argv[1]
+    try:
+        upload_file(file_name)
+    except (ClientException or ServerException) as err:
+        logging.error(err)
 
 
 if __name__ == '__main__':
-    run()
+    execute()
