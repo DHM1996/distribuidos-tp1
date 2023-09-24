921c46004ccb3f0e5413d87c0a8821628064c274
Nicolás Bugliot <nicobugliot@gmail.com>
2023-09-24 03:10:11 -0300

    add tests and selective repeat NOT FINISHED

--- src/lib/file_iterator.py
+++ src/lib/file_iterator.py
@@ -7,6 +7,15 @@ class FileIterator:
         if not os.path.isfile(file_path):
             raise FileNotFoundError
         self.file = open(file_path, 'rb')
+        self.file_path = file_path
+        self.file_data = []
+
+        with open(self.file_path, 'rb') as file:
+            while True:
+                data = file.read(self.read_size)
+                if not data:
+                    break
+                self.file_data.append(data)
 
     def __iter__(self):
         return self
@@ -16,4 +25,13 @@ class FileIterator:
         if not data:
             self.file.close()
             raise StopIteration
+        self.file_data.append(data)
         return data
+
+    def get_part(self, i):
+        if i < 0 or i >= len(self.file_data):
+            raise IndexError("Index out of range.")
+        return self.file_data[i]
+
+    def get_length(self):
+        return len(self.file_data)
---
+++ src/tests/selective_repeat_protocol_test.py
@@ -0,0 +1,47 @@
+import unittest
+import socket
+import threading
+import time
+
+from src.lib.selective_repeat_protocol import SelectiveRepeatSender, SelectiveRepeatReceiver
+
+
+class TestSelectiveRepeat(unittest.TestCase):
+    def setUp(self):
+        # Set up sender and receiver instances
+        self.sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sender_address = ("localhost", 12345)
+        self.receiver_address = ("localhost", 54321)
+        self.sender = SelectiveRepeatSender(self.sender_socket, self.receiver_address, window_size=5)
+        self.receiver = SelectiveRepeatReceiver(self.receiver_socket, self.receiver_address)
+
+    def tearDown(self):
+        # Clean up sender and receiver sockets
+        self.sender_socket.close()
+        self.receiver_socket.close()
+
+    def test_send_receive(self):
+        # Create a file with test data
+        test_data = b"Hello, Selective Repeat!"
+        with open("test_file.txt", "wb") as test_file:
+            test_file.write(test_data)
+
+        # Start receiver in a separate thread
+        receiver_thread = threading.Thread(target=self.receiver.receive)
+        receiver_thread.start()
+
+        # Send the file using the sender
+        with open("test_file.txt", "rb") as file:
+            self.sender.send_file(file.name)
+
+        # Wait for the receiver to finish
+        receiver_thread.join()
+
+        # Check if the received data matches the sent data
+        with open("received_file.txt", "rb") as received_file:
+            received_data = received_file.read()
+        self.assertEqual(received_data, test_data)
+
+if __name__ == "__main__":
+    unittest.main()
